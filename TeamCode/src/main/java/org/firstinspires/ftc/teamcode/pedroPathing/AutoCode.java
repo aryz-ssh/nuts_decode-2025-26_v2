fcce936259cc127aa5e02dafe5deaa0a0fbcaa40
programmingnuts5070 <ftcnuts5070@gmail.com>
2025-11-10 10:47:49 -0500

    pedropathing stuff updates-aarav

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoCode.java
@@ -9,13 +9,12 @@ import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Autonomous(name = "Pedro Pathing Autonomous", group = "Autonomous")
 @Configurable // Panels
-public class AutoCode extends LinearOpMode {
+public class AutoCode extends OpMode {
 
     private static final Pose pose1 =  new Pose(56.000, 8.000);
     private static final Pose pose2 = new Pose(39.988, 35.456);
@@ -41,35 +40,35 @@ public class AutoCode extends LinearOpMode {
     private static final Pose pose22 = new Pose(59.782, 83.529);
     private static final Pose pose23 = new Pose(59.782, 83.529);
     private static final Pose pose24 = new Pose(65.873, 77.873);
+    private TelemetryManager panelsTelemetry; // Panels Telemetry instance
+    public Follower follower; // Pedro Pathing follower instance
+    private int pathState; // Current autonomous path state (state machine)
+    private Paths paths; // Paths defined in the Paths class
 
-    public void runOpMode() throws InterruptedException {
+    @Override
+    public void init() {
+        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
 
-        TelemetryManager panelsTelemetry; // Panels Telemetry instance
-    Follower follower; // Pedro Pathing follower instance
-    int pathState; // Current autonomous path state (state machine)
-    Paths paths; // Paths defined in the Paths class
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
 
-    panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
-    follower = Constants.createFollower(hardwareMap);
-    follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
+        paths = new Paths(follower); // Build paths
 
-    paths = new Paths(follower); // Build paths
+        panelsTelemetry.debug("Status", "Initialized");
+        panelsTelemetry.update(telemetry);
+    }
 
-    panelsTelemetry.debug("Status", "Initialized");
-    panelsTelemetry.update(telemetry);
+    @Override
+    public void loop() {
+        follower.update(); // Update Pedro Pathing
+        pathState = autonomousPathUpdate(); // Update autonomous state machine
 
-        this.waitForStart();
-        while (opModeIsActive()) {
-            follower.update(); // Update Pedro Pathing
-            pathState = autonomousPathUpdate(); // Update autonomous state machine
-
-            // Log values to Panels and Driver Station
-            panelsTelemetry.debug("Path State", pathState);
-            panelsTelemetry.debug("X", follower.getPose().getX());
-            panelsTelemetry.debug("Y", follower.getPose().getY());
-            panelsTelemetry.debug("Heading", follower.getPose().getHeading());
-            panelsTelemetry.update(telemetry);
-        }
+        // Log values to Panels and Driver Station
+        panelsTelemetry.debug("Path State", pathState);
+        panelsTelemetry.debug("X", follower.getPose().getX());
+        panelsTelemetry.debug("Y", follower.getPose().getY());
+        panelsTelemetry.debug("Heading", follower.getPose().getHeading());
+        panelsTelemetry.update(telemetry);
     }
 
     public static class Paths {
@@ -196,7 +195,6 @@ public class AutoCode extends LinearOpMode {
         // Add your state machine Here
         // Access paths with paths.pathName
         // Refer to the Pedro Pathing Docs (Auto Example) for an example state machine
-        int pathState = 0;
         return pathState;
     }
 }

3e9c28432c3809c4b0c3eaba77734f47947dc75d
aryz-ssh <ajalvarez3099@gmail.com>
2025-12-06 12:57:34 -0500

    auto and stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocode/RedSideOffBigTriangle.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode.autocode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Mechanisms;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import java.util.ArrayList;
+
+@Autonomous(name = "RedSideOffBigTriangle", group = "Autonomous")
+@Configurable
+public class RedSideOffBigTriangle extends LinearOpMode {
+
+    private TelemetryManager panelsTelemetry;
+    private Follower follower;
+    private Paths paths;
+    private Mechanisms mechanisms;
+
+    private ArrayList<String> intakeOrder = new ArrayList<>();
+    private boolean intakeOn = false;
+    private long delayStart = 0;
+    private final long DELAY_MS = 400;
+
+    private String lastDetectedColor = null;
+
+    @Override
+    public void runOpMode() {
+
+        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(122.28196721311475, 123.6983606557377, Math.toRadians(217)));
+
+        paths = new Paths(follower);
+
+        mechanisms = new Mechanisms();
+        mechanisms.initMechanisms(hardwareMap, telemetry);
+
+        panelsTelemetry.debug("Status", "Initialized");
+        panelsTelemetry.update(telemetry);
+
+        waitForStart();
+
+        int state = 0;
+        while (opModeIsActive()) {
+
+            follower.update();
+
+            switch (state) {
+
+                case 0:
+                    follower.followPath(paths.Path1);
+                    state = 1;
+                    break;
+            }
+
+            panelsTelemetry.debug("State", state);
+            panelsTelemetry.debug("X", follower.getPose().getX());
+            panelsTelemetry.debug("Y", follower.getPose().getY());
+            panelsTelemetry.update(telemetry);
+        }
+    }
+
+    // ---------------- PATH LIST ----------------
+    public static class Paths {
+
+        public PathChain Path1;
+
+        public Paths(Follower follower) {
+            Path1 = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierCurve(
+                                    new Pose(121.102, 125.351),
+                                    new Pose(91.357, 101.744),
+                                    new Pose(89.233, 126.531)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(217), Math.toRadians(-90))
+                    .build();
+        }
+    }
+}
